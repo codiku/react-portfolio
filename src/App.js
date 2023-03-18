2c34f88aed1bda0b46dac1fa174a92068d6a33b9
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-03-18 11:15:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Box, Heading, HStack, Text } from "@chakra-ui/react";
 
 export default function App() {
   return <>
-    <Heading>This is a title using poppins font</Heading>
+    <Heading size="3xl">This is a title using poppins font</Heading>
     <Text>This is a text using inter font</Text>
     <HStack spacing={0}>
       <Box h={100} w={100} bg="primary.strong" />
