0783d0adee95ebdf1619cd1ee00750e59c18f25f
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-03-18 21:02:39 +0100

    wip

--- src/locales/i18next.js
+++ src/locales/i18next.js
@@ -2,8 +2,10 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import homeEN from "locales/en/home.json";
 import homeFR from "locales/fr/home.json";
+import LanguageDetector from "i18next-browser-languagedetector";
 i18n
     .use(initReactI18next)
+    .use(LanguageDetector)
     .init({
         resources: {
             en: {
@@ -13,7 +15,6 @@ i18n
                 home: homeFR
             }
         },
-        lng: "en",
         fallbackLng: "en",
 
         interpolation: {
