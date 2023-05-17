f387f147cfb75c8011a51a26580e09b67cf8a01b
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-05-17 18:11:54 +0200

    wip

--- src/locales/i18n-config.js
+++ src/locales/i18n-config.js
@@ -2,15 +2,18 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import homeEN from "@/locales/en/home.json";
 import homeFR from "@/locales/fr/home.json";
-
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      home: homeEN,
+import LanguageDetector from "i18next-browser-languagedetector";
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    resources: {
+      en: {
+        home: homeEN,
+      },
+      fr: {
+        home: homeFR,
+      },
     },
-    fr: {
-      home: homeFR,
-    },
-  },
-  fallbackLng: "en",
-});
+    fallbackLng: "en",
+  });
