68674b6c5121f47956d3d71647dcfcdc9c62206c
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-05-17 10:10:22 +0200

    Update Header.jsx

--- src/features/Header.jsx
+++ src/features/Header.jsx
@@ -1,23 +1,14 @@
-import { Flex, HStack, Image, Link } from "@chakra-ui/react";
-import bubbleImg from "@/assets/images/bubble.png";
+import { Flex, Image, Link } from "@chakra-ui/react";
 import logoImg from "@/assets/images/logo.png";
+import bubbleImg from "@/assets/images/bubble.png";
 import flagENImg from "@/assets/images/flag-en.png";
-
 export function Header() {
   return (
-    <Flex justify={"space-between"}>
+    <Flex justify="space-between">
+      <Image src={bubbleImg} h={10} />
       <Image src={logoImg} h={10} />
-      <HStack>
-        <Image src={bubbleImg} h={10} />
-        <Link
-          href="mailto:codiku.dev@gmail.com?subject=Contacting you from your portfolio"
-          fontSize="lg"
-          fontWeight="bold"
-        >
-          Hire me
-        </Link>
-        <Image pl={20} src={flagENImg} h={8} />
-      </HStack>
+      <Link>Hire me</Link>
+      <Image src={flagENImg} h={8} />
     </Flex>
   );
 }
