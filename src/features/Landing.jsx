99d7534770131bf50f534087f4eda3e7f28a1b31
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-05-17 11:16:37 +0200

    wip

--- src/features/Landing.jsx
+++ src/features/Landing.jsx
@@ -1,77 +1,40 @@
-import {
-  Badge,
-  Box,
-  Flex,
-  Heading,
-  Image,
-  Text,
-  Wrap,
-  WrapItem,
-} from "@chakra-ui/react";
-import developerImg from "@/assets/images/human.png";
-
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import humanImg from "@/assets/images/human.png";
 export function Landing() {
-  const mainText = (
-    <>
+  const leftSection = (
+    <Box>
       <Heading
         fontSize={{ base: "2xl", md: "4xl", xl: "7xl" }}
-        color={"secondary"}
+        color="secondary"
       >
-        Hi, My name
-        <Text>
-          is Codiku
-          <Box as="span" color="primary.dark">
-            .
-          </Box>
+        Hi, my name <br /> is Codiku
+        <Text as="span" color="primary.dark">
+          .
         </Text>
       </Heading>
-      <Text fontSize={"lg"} as={"span"} color="secondary">
-        I am
-        <Text as={"span"} fontWeight="bold">
-          {" "}
+      <Text fontSize="lg" color="secondary">
+        I am{" "}
+        <Text as="span" fontWeight="bold">
           a freelance developer/instructor
-        </Text>
-        <br />
-        located in Paris / France
+        </Text>{" "}
+        <br /> located in Paris/France
       </Text>
-    </>
+    </Box>
   );
 
-  const leftSection = <Box>{mainText}</Box>;
-
-  const badgeExperience = (
-    <Badge
-      w={160}
-      textAlign={"center"}
-      alignSelf="flex-end"
-      color="white"
-      bg="primary.light"
-      borderRadius={7}
-      pt={3}
-      pb={3}
-    >
-      <Text fontSize={"2xl"} color="black">
-        {new Date().getFullYear() - 2013}
-      </Text>
-      <Text color="black">years of experiences</Text>
-    </Badge>
-  );
   const rightSection = (
     <>
-      <Flex position="absolute" justify={"flex-end"}>
-        {badgeExperience}
-      </Flex>
-      <Image src={developerImg} w={400} />
+      <Image src={humanImg} w={400} />
     </>
   );
   return (
     <Flex
       direction={{ base: "column", md: "row" }}
-      justify={"space-evenly"}
+      justify="space-evenly"
       mt={{ base: 50, md: 150 }}
     >
       {leftSection}
-      <Box mt={{ base: 10, md: 0 }}>{rightSection}</Box>
+      {rightSection}
     </Flex>
   );
 }
