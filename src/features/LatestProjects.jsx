c85607566768590ad656b928e0b3aef2906fde9d
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-05-18 15:13:27 +0200

    wip

--- src/features/LatestProjects.jsx
+++ src/features/LatestProjects.jsx
@@ -1,13 +1,13 @@
-import { Box, Flex, Heading, Text, Wrap, WrapItem } from "@chakra-ui/react";
-import { useTranslation } from "react-i18next";
 import { ImageSlider } from "@/components/ImageSlider/ImageSlider";
+import { Box, Flex, Heading, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
 const FAKE_PROJECTS = [
   {
     id: 1,
     title: "Project 1",
-    type: "A project about this",
+    description: "A project about this",
     technologies: ["React", "ES6", "HTML", "CSS"],
     images: [
       "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
@@ -17,7 +17,7 @@ const FAKE_PROJECTS = [
   {
     id: 2,
     title: "Project 2",
-    type: "A project about that",
+    description: "A project about that",
     technologies: ["React", "Redux", "HTML", "CSS", "ES6"],
     images: [
       "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
@@ -27,7 +27,7 @@ const FAKE_PROJECTS = [
   {
     id: 3,
     title: "Project 3",
-    type: "A project about things",
+    description: "A project about things",
     technologies: [
       "React",
       "Redux",
@@ -43,36 +43,30 @@ const FAKE_PROJECTS = [
   },
 ];
 export function LatestProjects() {
-  const { t } = useTranslation("home");
   const [projects, setProjects] = useState(FAKE_PROJECTS);
-
-  const renderProject = (project) => {
+  const { t } = useTranslation("home");
+  const renderProject = ({ id, images, title, description }) => {
     return (
-      <WrapItem key={project.id}>
-        <Box>
-          <ImageSlider imageList={project.images} />
-          <Heading size="md" color="secondary" mt={3}>
-            <Box
-              display={"inline-block"}
-              verticalAlign="middle"
-              mr={3}
-              bg="primary.dark"
-              w={25}
-              h={1}
-            />
-            {project.title}
-          </Heading>
-          <Text>{project.type}</Text>
-        </Box>
+      <WrapItem key={id} flexDir={"column"}>
+        <ImageSlider imageList={images} />
+        <Heading size="md" color="secondary" mt={3}>
+          <Box
+            display={"inline-block"}
+            bg="primary.dark"
+            w={25}
+            h={1}
+            mr={3}
+            verticalAlign="middle"
+          />
+          {title}
+        </Heading>
+        <Text>{description}</Text>
       </WrapItem>
     );
   };
-
   return (
     <Flex direction={"column"} w="100%">
-      <Heading color={"secondary"} size="2xl">
-        {t("latestProjects")}
-      </Heading>
+      <Heading>{t("latestProjects")}</Heading>
       <Wrap mt={10} spacing={16}>
         {projects?.map(renderProject)}
       </Wrap>
