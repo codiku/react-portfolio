dbe3073cc531d2e197ca1ec8e099a892aa89cf37
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-05-18 17:55:13 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { Box } from "@chakra-ui/react";
 import { Header } from "./features/Header";
 import { Landing } from "./features/Landing";
 import { LatestProjects } from "./features/LatestProjects";
-
+import { Footer } from "@/features/Footer";
 export function App() {
   return (
-    <Box p={20}>
-      <Header />
-      <Landing />
-      <LatestProjects />
-    </Box>
+    <>
+      <Box p={20}>
+        <Header />
+        <Landing />
+        <LatestProjects />
+      </Box>
+      <Footer />
+    </>
   );
 }
