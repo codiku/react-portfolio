9af75b4f22241b75dc384e1f13cffbc28eed5584
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-05-18 13:37:24 +0200

    wip

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -1,16 +1,34 @@
 import "react-slideshow-image/dist/styles.css";
 import { Slide } from "react-slideshow-image";
-import { Box, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import { useState } from "react";
 
 export function ImageSlider({ imageList }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const openImg = (i) => {
+    setCurrentIndex(i);
+    setIsModalOpen(true);
+  };
+
   const renderImages = () => {
-    return imageList.map((imageLink) => {
+    return imageList.map((imageLink, i) => {
       return (
         <Image
           src={imageLink}
           boxShadow="xl"
           borderRadius="xl"
           key={imageLink}
+          onClick={() => openImg(i)}
         />
       );
     });
@@ -20,9 +38,44 @@ export function ImageSlider({ imageList }) {
       {renderImages()}
     </Slide>
   );
+
+  const modalSlider = (
+    <Modal
+      closeOnOverlayClick={true}
+      isOpen={isModalOpen}
+      onClose={() => setIsModalOpen(false)}
+      size="6xl"
+    >
+      <ModalOverlay />
+      <ModalContent bg="transparent" boxShadow={"none"}>
+        <ModalCloseButton
+          backgroundColor="white"
+          borderColor={"black"}
+          borderWidth={2}
+          mt={-2}
+          zIndex={10}
+          onClick={() => setCurrentIndex(0)}
+        />
+
+        <ModalBody justifyContent={"center"} alignItems={"center"} flex>
+          {currentIndex !== undefined && (
+            <Slide
+              autoplay={false}
+              defaultIndex={currentIndex}
+              arrows={imageList.length > 1}
+            >
+              {renderImages()}
+            </Slide>
+          )}
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+
   return (
     <>
       <Box w={350}>{slider}</Box>
+      {modalSlider}
     </>
   );
 }
