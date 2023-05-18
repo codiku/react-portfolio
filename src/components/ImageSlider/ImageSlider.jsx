a72e3b86c768dc0e9084d57924a3cef2c24da8aa
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-05-18 13:53:45 +0200

    wip

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -13,65 +13,50 @@ import { useState } from "react";
 
 export function ImageSlider({ imageList }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-  const openImg = (i) => {
-    setCurrentIndex(i);
+  const openModalSlider = (imgIndex) => {
+    setCurrentImageIndex(imgIndex);
     setIsModalOpen(true);
   };
-
   const renderImages = () => {
     return imageList.map((imageLink, i) => {
       return (
         <Image
+          onClick={() => openModalSlider(i)}
           src={imageLink}
           boxShadow="xl"
           borderRadius="xl"
           key={imageLink}
-          onClick={() => openImg(i)}
         />
       );
     });
   };
   const slider = (
-    <Slide infinite={false} arrows={imageList.length > 1} autoplay={false}>
+    <Slide
+      defaultIndex={currentImageIndex}
+      infinite={false}
+      arrows={imageList.length > 1}
+      autoplay={false}
+    >
       {renderImages()}
     </Slide>
   );
 
   const modalSlider = (
     <Modal
-      closeOnOverlayClick={true}
+      bg="transparent"
+      size="6xl"
       isOpen={isModalOpen}
       onClose={() => setIsModalOpen(false)}
-      size="6xl"
     >
       <ModalOverlay />
       <ModalContent bg="transparent" boxShadow={"none"}>
-        <ModalCloseButton
-          backgroundColor="white"
-          borderColor={"black"}
-          borderWidth={2}
-          mt={-2}
-          zIndex={10}
-          onClick={() => setCurrentIndex(0)}
-        />
-
-        <ModalBody justifyContent={"center"} alignItems={"center"} flex>
-          {currentIndex !== undefined && (
-            <Slide
-              autoplay={false}
-              defaultIndex={currentIndex}
-              arrows={imageList.length > 1}
-            >
-              {renderImages()}
-            </Slide>
-          )}
-        </ModalBody>
+        <ModalCloseButton zIndex={1} bg="white" />
+        <ModalBody>{slider}</ModalBody>
       </ModalContent>
     </Modal>
   );
-
   return (
     <>
       <Box w={350}>{slider}</Box>
