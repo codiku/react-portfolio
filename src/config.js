85c1be001bd48c5cb5df2cb909fefeeae8c5a06a
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-05-19 14:28:23 +0200

    wip

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAIjrcY9loE5YzUSRbTzTxEWV1f_GH_PjM",
+  authDomain: "portfolio-tv5few.firebaseapp.com",
+  projectId: "portfolio-tv5few",
+  storageBucket: "portfolio-tv5few.appspot.com",
+  messagingSenderId: "254834398820",
+  appId: "1:254834398820:web:5096dd45fbc2c84167b171",
+};
--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,10 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import { myTheme } from "@/styles/theme.js";
+import { FirebaseApp } from "./utils/firebase.js";
+
+FirebaseApp.init();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ChakraProvider theme={myTheme}>
     <App />
